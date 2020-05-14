0e329f2eda73cc796720735e04287a8dab2c9dc9
forestcorgii <seanivanf@gmail.com>
2020-05-14 23:20:31 +0800

    release

--- online_floorpass_project/online_floorpass_project/settings.py
+++ online_floorpass_project/online_floorpass_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'epi-2w#kuhksq&lfux^c(*zv80p2ml6wh4$kjqffv4g(u3rd@@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['104.197.214.104']
+ALLOWED_HOSTS = ['104.197.214.104']
 
 
 # Application definition
